6a35ec0c741c990addd313dda5291dcc0032986c
Logan Losee <logan.losee99@gmail.com>
2023-09-10 20:14:23 -0600

    java scripts written

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 // Assignment code here
+function generatePassword() {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numericChars = '0123456789';
+  const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
+  const length = parseInt(prompt('Enter password length between 8 and 128 characters:'));
+
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert('Password length must be between 8 and 128 characters.');
+    return;
+  }
+
+  const includeLowercase = confirm('Include lowercase characters?');
+  const includeUppercase = confirm('Include uppercase characters?');
+  const includeNumeric = confirm('Include numeric characters?');
+  const includeSpecial = confirm('Include special characters?');
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert('At least one character type must be selected.');
+    return;
+  }
+
+  let allChars = '';
+  if (includeLowercase) allChars += lowercaseChars;
+  if (includeUppercase) allChars += uppercaseChars;
+  if (includeNumeric) allChars += numericChars;
+  if (includeSpecial) allChars += specialChars;
+
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +51,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
