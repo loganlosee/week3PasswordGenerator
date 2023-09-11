6db5dc69eaabe831ebf1d53d9e40a5429a6647da
Logan Losee <logan.losee99@gmail.com>
2023-09-10 20:50:28 -0600

    added annotations to .js file to explain code

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-// Assignment code here
+// declare variables for character sets.
+
 function generatePassword() {
   const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
   const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const numericChars = '0123456789';
   const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
+  // prompt for password length, then check password length is a valid int.
+
   const length = parseInt(prompt('Enter password length between 8 and 128 characters:'));
 
   if (isNaN(length) || length < 8 || length > 128) {
     alert('Password length must be between 8 and 128 characters.');
     return;
+    
   }
+
+  // prompt for choosing character sets, then verify atleast one set is chosen.
 
   const includeLowercase = confirm('Include lowercase characters?');
   const includeUppercase = confirm('Include uppercase characters?');
@@ -20,7 +26,10 @@ function generatePassword() {
   if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
     alert('At least one character type must be selected.');
     return;
+
   }
+
+  // check and append included character sets.
 
   let allChars = '';
   if (includeLowercase) allChars += lowercaseChars;
@@ -28,13 +37,19 @@ function generatePassword() {
   if (includeNumeric) allChars += numericChars;
   if (includeSpecial) allChars += specialChars;
 
+  // add digits 1 by 1 at random within included character sets until specified length is met.
+
   let password = '';
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * allChars.length);
     password += allChars.charAt(randomIndex);
+
   }
 
+  // send generated password to quieried css object #password
+
   return password;
+
 }
 
 // Get references to the #generate element
